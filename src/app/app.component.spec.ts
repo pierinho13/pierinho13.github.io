5f3fcbc3a1e3910ae85c3572782bfc09afed9276
pierinho13@gmail.com <pierinho13@gmail.com>
2022-12-27 21:46:26 +0100

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppTopBarComponent } from './app.topbar.component';
+import { AppBreadcrumbComponent } from './app.breadcrumb.component';
+import { AppProfileComponent } from './app.profile.component';
+import { BreadcrumbService } from './app.breadcrumb.service';
+import { AppFooterComponent } from './app.footer.component';
+import { AppMenuComponent } from './app.menu.component';
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'piero'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('piero');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('piero app is running!');
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [NoopAnimationsModule, RouterTestingModule, AccordionModule, PanelModule],
+            declarations: [
+                AppComponent,
+                AppTopBarComponent,
+                AppMenuComponent,
+                AppProfileComponent,
+                AppFooterComponent,
+                AppBreadcrumbComponent
+            ],
+            providers: [BreadcrumbService]
+        }).compileComponents();
+    }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'piero';
+export class AppComponent implements OnInit{
+
+    layoutMode = 'static';
+
+    darkMenu = true;
+
+    profileMode = 'popup';
+
+    inputStyle = 'outlined';
+
+    ripple: boolean;
+
+    constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
 }
